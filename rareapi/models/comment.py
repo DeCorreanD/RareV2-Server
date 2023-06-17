8077107adf294be18a027d3240b574e89574079b
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-06-17 11:38:55 -0500

    made the tags and post tags model

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Comments(models.Model):
+class Comment(models.Model):
 
     post_id = models.CharField(max_length=50)
-    artist_id = models.CharField(max_length=50)
+    author_id = models.CharField(max_length=50)
     content = models.CharField(max_length=50)
     created_on = models.DateTimeField(null=True, blank=True)
     
