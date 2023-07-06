baf38522b7ef5234039479914ef571946daa8640
Evan Breland <evan.breland@gmail.com>
2023-07-05 19:42:42 -0500

    updated userviews and sub model

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -2,7 +2,7 @@ from django.db import models
 from .user import User
 
 class Subscription(models.Model):
-    follower_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower_id")
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_id")
+    follower_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
     created_on = models.DateField()
     ended_on = models.DateField()
